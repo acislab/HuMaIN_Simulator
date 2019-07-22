7d00a8c0969bf8794ac796baa3e5ae4d92f82b14
ialzuru <ialzuru@gmail.com>
2019-07-22 10:55:18 -0400

    create_sim_set v. 0.2

--- humain/common/create_sim_set.py
+++ humain/common/create_sim_set.py
@@ -4,13 +4,13 @@ from utils import *
 from constants import *
 
 
-# function to find the action and replace with passed param
+# function to find the task and replace with passed param
 def read_sim_file(param):
-	regex_str = " "+args.parameter+" = "
+	regex_str = " " + args.argument + " = "
 	temp_var = -1
 	for ele in data_list:
-		# search for line which has the action to be changed
-		if ele[0] == args.actions:
+		# search for line which has the task to be changed
+		if ele[0] == args.task:
 			for var in ele:
 				if re.match(r"%s[a-zA-Z0-9]*" % regex_str, var):
 					temp_var = ele.index(var)
@@ -59,11 +59,10 @@ if __name__ == '__main__':
 	"""
 	parser = argparse.ArgumentParser("Generate random values for the files in a directory.")
 	parser.add_argument('-p', '--project', action="store", required=True, help="Project selected")
-	parser.add_argument('-w', '--workflow', action="store", required=False, help=" Workflow selected")
 	parser.add_argument('-s', '--simulation_file', action="store", required=True, help="Simulation to edit")
-	parser.add_argument('-a', '--actions', action="store", required=True, help="Action to edit")
-	parser.add_argument('-d', '--parameter', action="store", required=True, help="Action's parameter to change")
-	parser.add_argument('-v', '--variable', action="append", required=True, help="Value to put for the parameter selected, can send in more than one.")
+	parser.add_argument('-t', '--task', action="store", required=True, help="Task to edit")
+	parser.add_argument('-a', '--argument', action="store", required=True, help="Task's parameter or argument to change")
+	parser.add_argument('-v', '--value', action="append", required=True, help="Value to put for the parameter selected, can send in more than one.")
 	parser.add_argument('-o', '--output_file', action="store", required=True, help="Output csv file for the new simulation filename")
 	args = parser.parse_args()
 
@@ -73,13 +72,14 @@ if __name__ == '__main__':
 	# verify the creation of the output file. 
 	verify_create_file( output_dir, 'The output file could not be created.', parser, 4 )
 
+	# Usage: ./create_sim_set.py -p selfie -w event_date -t ocr -a dataset -v datasets/aocr_mix100/ocr/ocropus -v datasets/aocr_mix100/ocr/tesseract -v datasets/aocr_mix100/ocr/gc-ocr -o sim_set_001
 	# usage: python3 common/create_sim_set.py -p selfie -w event_date -s event_date_001.csv -a ocr_dataset -d dataset -v datasets/aocr_mix100/ocr/tersseract -v datasets/aocr_mix100/ocr/g_ocr -o new_sim.csv
 	# usage: python3 common/create_sim_set.py -p selfie -w event_date -s event_date_001.csv -a ocr_dataset -d dataset -v datasets/aocr_mix100/ocr/tersseract -o new_sim.csv
 	# usage: python3 common/create_sim_set.py -p selfie -w event_date -s event_date_001.csv -a crowd_event_date_dataset -d crowd_data -v HELLOWORLD -o new_sim.csv
 
 	lines = read_section_lines(simulation_file, "[TASKS]")
 	data = lines.split("\n")
-	# has data for each action 
+	# has data for each Task 
 	data_list = []
 	f = open(output_dir, "w+")
 	
@@ -88,16 +88,10 @@ if __name__ == '__main__':
 		data_list.append(temp_list)
 
 	iter_no = 1
-	for param in args.variable:
+	for param in args.value:
 		read_sim_file(param)
 		combine_and_write(iter_no, data_list)
 		iter_no += 1
 
 	copy_remaining_file("[METRICS]")
 	copy_remaining_file("[POST-PROCESSING]")
-
-	
-
-
-
-
