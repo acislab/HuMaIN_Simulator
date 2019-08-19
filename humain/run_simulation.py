9805482c11ae6f31a0dac22ffc7cab3e0389b4a2
ialzuru <ialzuru@gmail.com>
2019-08-18 22:59:08 -0400

    Scientific Name by suffix

--- humain/run_simulation.py
+++ humain/run_simulation.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
 
 	# Usage example
 	# python3 run_simulation.py -p selfie -w event_date -s event_date_001
+	# python3 run_simulation.py -p selfie -w scientific_name -s scientific_name
 	
 	sim = Simulation( args.project, args.workflow, args.sim_file )
 	#sim.draw_workflow()
